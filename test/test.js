833a20f68087b3d2ee33f68990f2cbd9017fcd59
Joakim Olsson <joakim.olsson@adicitus.cloud>
2022-02-03 16:09:46 +0100

    Fixed package name and added tests.

---
+++ test/test.js
@@ -0,0 +1,180 @@
+let JWTGenerator = require('..')
+let jwt = require('jsonwebtoken')
+let {v4: uuidv4} = require('uuid')
+let assert = require('assert')
+
+describe('Default Generator', () => {
+    let generator = new JWTGenerator()
+
+    describe('newToken(clientId, options)', () => {
+        let result = null
+
+        it('Should return an object.', async () => {
+            result = await generator.newToken('client1')
+            assert.ok(result)
+        })
+
+        it('Should generate a token and associated token record.', () => {
+            assert.ok(result.record)
+            assert.ok(result.token)
+        })
+        
+        it('Issuer should be this generator', () => {
+            assert.equal(result.record.issuer, generator.id)
+        })
+        
+        it('Token should be valid JWT', () => {
+            let {header, payload} = jwt.decode(result.token, {complete: true})
+            assert.ok(header)
+            assert.ok(payload)
+        })
+
+        it('header.kid should be same as record ID', () => {
+            let {header, payload} = jwt.decode(result.token, {complete: true})
+            assert.equal(header.kid, result.record.id)
+        })
+
+        it('payload.iss should be same as record issuer', () => {
+            let {header, payload} = jwt.decode(result.token, {complete: true})
+            assert.equal(payload.iss, result.record.issuer)
+        })
+
+        it('payload.sub should be same as record subject', () => {
+            let payload = jwt.decode(result.token)
+            assert.equal(payload.sub, result.record.subject)
+        })
+
+        it('Should accept custom duration.', async () => {
+            let {token, record} = await generator.newToken('client1', { duration: {minutes: 30} })
+            let durationMillis = record.expires.toMillis() - record.issued.toMillis()
+            assert.equal(durationMillis, (30 * 60 * 1000))
+        })
+
+        it('Should accept custom claims.', async () => {
+            let {token, record} = await generator.newToken('client1', { payload: { testClaim: 'test'} })
+            let payload = jwt.decode(token)
+            assert.ok(payload.testClaim)
+            assert.equal(payload.testClaim, 'test')
+        })
+
+        it('Should not overwrite fixed claims (iss, sub).', async () => {
+            let {token, record} = await generator.newToken('client1', { payload: { iss: 'wrongIssuer', sub: 'wrongSubject'} })
+            let payload = jwt.decode(token)
+            assert.ok(payload.iss)
+            assert.ok(payload.sub)
+            assert.equal(payload.iss, record.issuer)
+            assert.equal(payload.sub, record.subject)
+        })
+
+    })
+
+    describe('generateKeys()', () => {
+        it('Should create a new key pair.', async () => {
+            let result1 = await generator.newToken('client2')
+            generator.generateKeys()
+            let result2 = await generator.newToken('client2')
+            assert.notEqual(result1.record.key, result2.record.key)
+        })
+    })
+
+    describe('verifyToken(token, options)', () => {
+        describe('If successful:', () => {
+            let clientId = 'client1'
+            let tokenResult = null
+            let verifyResult = null
+            let customClaim1 = "TEST1"
+
+            it('Should validate a correct token against its record.', async () => {
+                tokenResult = await generator.newToken(clientId, { payload: { customClaim1: customClaim1 } })
+                verifyResult = await generator.verifyToken(tokenResult.token, { record: tokenResult.record })
+                assert.ok(verifyResult.success)
+            })
+
+            it('Should return the subject in the result.', () => {
+                assert.equal(verifyResult.subject, clientId)
+            })
+
+            it('Should return the payload in the result.', () => {
+                assert.ok(verifyResult.payload)
+            })
+
+            it('Payload should retain custom claims.', () => {
+                assert.equal(verifyResult.payload.customClaim1, customClaim1)
+            })
+        })
+
+        describe('If failed:', () => {
+
+            it('Should fail for undefined token.', async () => {
+                let r = await generator.verifyToken(undefined)
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidTokenError')
+            })
+
+            it('Should fail for null token.', async () => {
+                let r = await generator.verifyToken(null)
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidTokenError')
+            })
+
+            it('Should fail for empty token.', async () => {
+                let r = await generator.verifyToken('')
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidTokenError')
+            })
+
+            it('Should fail for non-token string.', async () => {
+                let r = await generator.verifyToken(uuidv4())
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidTokenError')
+            })
+
+            it('Should fail for old tokens.', async () =>{
+                let r = generator.newToken('oldClient1', { duration: { seconds: -5 } })
+                r = await generator.verifyToken(r.token, { record: r.record })
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidTokenError')
+            })
+
+            it('Should fail with wrong record.', async () => {
+                let r1 = await generator.newToken('client1')
+                let r2 = await generator.newToken('client2')
+                let r = await generator.verifyToken(r1.token, { record: r2.record })
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidTokenError')
+            })
+
+            it('Should fail with no record.', async () => {
+                let r = await generator.newToken('client2')
+                r = await generator.verifyToken(r.token)
+                assert.ok(!r.success)
+                assert.equal(r.status, 'noRecordError')
+            })
+
+            it('Should fail with invalid record (no subject).', async () => {
+                let r = await generator.newToken('client1')
+                delete r.record.subject
+                r = await generator.verifyToken(r.token, { record: r.record })
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidRecordError')
+            })
+
+            it('Should fail with invalid record (no issuer).', async () => {
+                let r = await generator.newToken('client1')
+                delete r.record.issuer
+                r = await generator.verifyToken(r.token, { record: r.record })
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidRecordError')
+            })
+
+            it('Should fail with invalid record (no key).', async () => {
+                let r = await generator.newToken('client1')
+                delete r.record.key
+                r = await generator.verifyToken(r.token, { record: r.record })
+                assert.ok(!r.success)
+                assert.equal(r.status, 'invalidRecordError')
+            })
+        })
+    })
+
+})
