fd2bd69c1e6a854c090fe8b35170e2ac85cc91f5
Joakim Olsson <joakim.olsson@adicitus.cloud>
2022-02-04 10:23:15 +0100

    Added tests for non-default generators.

--- test/test.js
+++ test/test.js
@@ -29,12 +29,12 @@ describe('Default Generator', () => {
             assert.ok(payload)
         })
 
-        it('header.kid should be same as record ID', () => {
+        it('header.kid should be same as record.id', () => {
             let {header, payload} = jwt.decode(result.token, {complete: true})
             assert.equal(header.kid, result.record.id)
         })
 
-        it('payload.iss should be same as record issuer', () => {
+        it('payload.iss should be same as record.issuer', () => {
             let {header, payload} = jwt.decode(result.token, {complete: true})
             assert.equal(payload.iss, result.record.issuer)
         })
@@ -45,9 +45,11 @@ describe('Default Generator', () => {
         })
 
         it('Should accept custom duration.', async () => {
-            let {token, record} = await generator.newToken('client1', { duration: {minutes: 30} })
+            let {token, record} = await generator.newToken('client1', { duration: {seconds: 30} })
             let durationMillis = record.expires.toMillis() - record.issued.toMillis()
-            assert.equal(durationMillis, (30 * 60 * 1000))
+            let payload = jwt.decode(token)
+            assert.equal(durationMillis, (30 * 1000))
+            assert.equal(payload.exp - payload.iat, 30)
         })
 
         it('Should accept custom claims.', async () => {
@@ -98,7 +100,7 @@ describe('Default Generator', () => {
                 assert.ok(verifyResult.payload)
             })
 
-            it('Payload should retain custom claims.', () => {
+            it('Should retain custom claims in payload.', () => {
                 assert.equal(verifyResult.payload.customClaim1, customClaim1)
             })
         })
@@ -129,7 +131,7 @@ describe('Default Generator', () => {
                 assert.equal(r.status, 'invalidTokenError')
             })
 
-            it('Should fail for old tokens.', async () =>{
+            it('Should fail on expired tokens.', async () =>{
                 let r = generator.newToken('oldClient1', { duration: { seconds: -5 } })
                 r = await generator.verifyToken(r.token, { record: r.record })
                 assert.ok(!r.success)
@@ -177,4 +179,56 @@ describe('Default Generator', () => {
         })
     })
 
+})
+
+describe('Custom Generator', () => {
+
+    it('Should accept a custom ID.', () => {
+        let id = uuidv4()
+        let g = new JWTGenerator({ id: id })
+        assert.equal(g.id, id)
+    })
+
+    describe('KeyLifetime', () => {
+
+        describe('0 (Nonce)', () => {
+            let g = new JWTGenerator({ id: 'CustomGenerator', keyLifetime: {minutes: 0} })
+
+            it('Should generate a new key for eaach token.', async () => {
+                let r1 = await g.newToken('client1')
+                let r2 = await g.newToken('client1')
+
+                assert.notEqual(r1.record.key, r2.record.key)
+                assert.ok( (await g.verifyToken(r1.token, { record: r1.record })).success )
+                assert.ok( (await g.verifyToken(r2.token, { record: r2.record })).success )
+            })
+        })
+
+        describe('200ms', () => {
+            let g = new JWTGenerator({ id: 'CustomGenerator', keyLifetime: {milliseconds: 200} })
+
+            it('Should regenerate keys after 200ms seconds.', async () => {
+                let r1 = await g.newToken('client1')
+                let r2 = await g.newToken('client1')
+                await new Promise(resolve => setTimeout(resolve, 200));
+                let r3 = await g.newToken('client1')
+
+                assert.equal(r1.record.key, r2.record.key)
+                assert.notEqual(r1.record.key, r3.record.key)
+                assert.ok( (await g.verifyToken(r1.token, { record: r1.record })).success )
+                assert.ok( (await g.verifyToken(r2.token, { record: r2.record })).success )
+                assert.ok( (await g.verifyToken(r3.token, { record: r3.record })).success )
+            })
+        })
+    })
+
+    describe('TokenLifetime: 30 seconds', () => {
+        let g = new JWTGenerator({ id: 'CustomGenerator', tokenLifetime: {seconds: 30} })
+
+        it ('Should generate tokens with a lifetime of 30s.', async () => {
+            let r = await g.newToken('client1')
+            let payload = jwt.decode(r.token)
+            assert.equal( (payload.exp - payload.iat), 30)
+        })
+    })
 })
